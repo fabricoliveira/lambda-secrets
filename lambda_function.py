d37237f0c1e4c0043444248a07de3b7c537c9794
Fabricio Oliveira <43307831+fabricoliveira@users.noreply.github.com>
2023-03-05 04:27:18 -0300

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -6,23 +6,16 @@ from aws_lambda_powertools.utilities import parameters
 
 def lambda_handler(event, context):
     response = {}
-    credentials = ''
-    url = ''
-    user = ''
-    token = ''
 
-    if event['method'] == 'boto3':
-        print('************ executando boto3 ***************')
-        client = boto3.client('ssm')
-        credentials = client.get_parameter(Name='/IntegracaoDigital/Gateway/Mainframe/credentials')['Parameter']['Value']
-        url = client.get_parameter(Name='/IntegracaoDigital/Gateway/Mainframe/url')['Parameter']['Value']
-    else:
-        print('************ executando AWS lambda powertools ***************')
-        url = parameters.get_parameter('/IntegracaoDigital/Gateway/Mainframe/url')
-        credentials = parameters.get_parameter('/IntegracaoDigital/Gateway/Mainframe/credentials')
-        credentials = json.loads(parameters.get_secret(credentials))
-        user = credentials['gateway-mainframe-ims-user']
-        token = credentials['gateway-mainframe-ims-token']
+
+    url = parameters.get_parameter('/IntegracaoDigital/Gateway/Mainframe/url')
+    
+
+
+    credentials = parameters.get_parameter('/IntegracaoDigital/Gateway/Mainframe/credentials')
+    credentials = json.loads(parameters.get_secret(credentials))
+    user = credentials['gateway-mainframe-ims-user']
+    token = credentials['gateway-mainframe-ims-token']
 
     response = {
         'credentials': credentials,
@@ -30,5 +23,4 @@ def lambda_handler(event, context):
         'user': user,
         'token': token
     }
-
     return response
