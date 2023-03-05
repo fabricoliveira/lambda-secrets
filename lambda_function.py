f7759353ab3dd7fdfbce8a2f0f004e73ed4bce06
Fabricio Oliveira <43307831+fabricoliveira@users.noreply.github.com>
2023-03-05 01:42:46 -0300

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -6,14 +6,22 @@ from aws_lambda_powertools.utilities import parameters
 client = boto3.client('ssm')
 
 def lambda_handler(event, context):
-    response = ''
-    print(event['method'])
+    response = {}
+    arn = ''
+    url = ''
+
     if event['method'] == 'boto3':
         print('************ executando boto3 ***************')
-        response = client.get_parameter(Name='/IntegracaoDigital/Gateway/Mainframe/credentials')
-        response = response['Parameter']['Value']
+        arn = client.get_parameter(Name='/IntegracaoDigital/Gateway/Mainframe/credentials')['Parameter']['Value']
+        url = client.get_parameter(Name='/IntegracaoDigital/Gateway/Mainframe/url')['Parameter']['Value']
     else:
         print('************ executando AWS lambda powertools ***************')
-        response = parameters.get_parameter('/IntegracaoDigital/Gateway/Mainframe/credentials')
+        arn = parameters.get_parameter('/IntegracaoDigital/Gateway/Mainframe/credentials')
+        url = parameters.get_parameter('/IntegracaoDigital/Gateway/Mainframe/url')
+
+    response = {
+        'arn':arn,
+        'url': url
+    }
 
     return response
