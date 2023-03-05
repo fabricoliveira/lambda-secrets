c3ee8e554a2f439f953627737e019c4269ace6d8
Fabricio Oliveira <43307831+fabricoliveira@users.noreply.github.com>
2023-03-05 00:48:41 -0300

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -5,7 +5,7 @@ import boto3
 def lambda_handler(event, context):
     response = ''
     print(event['method'])
-    if event['method'] is 'boto3':
+    if event['method'] == 'boto3':
         print('************ executando boto3 ***************')
         client = boto3.client('ssm')
         response = client.get_parameter('/IntegracaoDigital/Gateway/Mainframe/credentials')
